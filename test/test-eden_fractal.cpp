ca20665a12ee8685b18897dc3f88c7a7fd0f38cf
James-Mart <jmartx93@gmail.com>
2022-06-09 14:40:59 +0000

    add more unit tests to confirm functionality

--- test/test-eden_fractal.cpp
+++ test/test-eden_fractal.cpp
@@ -56,6 +56,15 @@ bool succeeded(const transaction_trace& trace)
     return (trace.status == transaction_status::executed);
 }
 
+bool failed(const transaction_trace& trace)
+{
+    if (trace.except) {
+        UNSCOPED_INFO("transaction has exception: " << *trace.except << "\n");
+    }
+
+    return (trace.status != transaction_status::executed);
+}
+
 bool failedWith(const transaction_trace& trace, std::string_view err)
 {
     bool ret = (trace.except->find(err.data()) != string::npos);
@@ -264,6 +273,7 @@ SCENARIO("Rank submission")
         setup_createAccounts(t);
         setup_token(t);
 
+        auto alice = t.as("alice"_n);
         auto dan = t.as("dan"_n);
 
         // clang-format off
@@ -299,15 +309,165 @@ SCENARIO("Rank submission")
             auto submitRanks = dan.trace<actions::submitranks>(util::from_json<AllRankings>(ranks));
             CHECK(succeeded(submitRanks));
         }
+        THEN("A non-admin may not submit a ranking")
+        {
+            auto submitRanks = alice.trace<actions::submitranks>(util::from_json<AllRankings>(ranks));
+            CHECK(failedWith(submitRanks, requiresAdmin));
+        }
 
-        /* TODO - Add test cases to confirm all error conditions:
-         *   Too few members in a group
-         *   Too many members in a group
-         *   Too few groups
-         *   One name repeated in multiple groups
-        */
+        THEN("A ranking with too few groups cannot be submitted")
+        {
+            AllRankings ar{{{{"james"_n, "dan"_n, "alice"_n, "bob"_n, "charlie"_n, "igor"_n}}}};
+            auto submitRanks = dan.trace<actions::submitranks>(ar);
+            CHECK(failedWith(submitRanks, too_few_groups));
+        }
+        THEN("A ranking with a group of too few members cannot be submitted")
+        {
+            AllRankings ar{{{{"james"_n, "dan"_n, "alice"_n, "bob"_n}}, {{"david"_n, "elaine"_n, "frank"_n, "gary"_n, "harry"_n, "jenny"_n}}}};
+            auto submitRanks = dan.trace<actions::submitranks>(ar);
+            CHECK(failedWith(submitRanks, group_too_small));
+        }
+        THEN("A ranking with a group of too many memmbers cannot be submitted")
+        {
+            AllRankings ar{{{{"james"_n, "dan"_n, "alice"_n, "bob"_n, "charlie"_n, "igor"_n, "kathy"_n}}, {{"david"_n, "elaine"_n, "frank"_n, "gary"_n, "harry"_n, "jenny"_n}}}};
+            auto submitRanks = dan.trace<actions::submitranks>(ar);
+            CHECK(failedWith(submitRanks, group_too_large));
+        }
+        THEN("A ranking with two groups that share a person cannot be submitted")
+        {
+            AllRankings ar{{{{"james"_n, "dan"_n, "alice"_n, "bob"_n, "charlie"_n, "igor"_n}}, {{"james"_n, "elaine"_n, "frank"_n, "gary"_n, "harry"_n, "jenny"_n}}}};
+            auto submitRanks = dan.trace<actions::submitranks>(ar);
+            CHECK(failed(submitRanks));  // Error message is dynamic
+        }
     }
 }
 
-// Could add some tests for:
-// SCENARIO("Reward customization")
+SCENARIO("Reward distribution")
+{
+    GIVEN("Standard setup, and an admin has a ranking to submit")
+    {
+        // This starts a single-producer chain
+        test_chain t;
+        setup_installMyContract(t);
+        setup_createAccounts(t);
+        setup_token(t);
+
+        auto dan = t.as("dan"_n);
+
+        // Give the eden fractal some EOS
+        t.as("eosio"_n).act<token::actions::issue>("eosio"_n, s2a("1000000.0000 EOS"), "");
+        t.as("eosio"_n).act<token::actions::transfer>("eosio"_n, default_contract_account, s2a("10000.0000 EOS"), "");
+
+        AllRankings ranks{{{{"james"_n, "dan"_n, "alice"_n, "bob"_n, "charlie"_n, "igor"_n}}, {{"david"_n, "elaine"_n, "frank"_n, "gary"_n, "harry"_n, "jenny"_n}}}};
+
+        auto getJamesEden = []() {
+            fractal_contract::accounts accountstable(eden_fractal::default_contract_account, "james"_n.value);
+            auto itr = accountstable.find(eden_fractal::eden_symbol.code().raw());
+            if (itr == accountstable.end()) {
+                return (int64_t)0;
+            }
+            else {
+                return itr->balance.amount;
+            }
+        };
+
+        auto getJamesEOS = []() {
+            token::contract::accounts accountstable("eosio.token"_n, "james"_n.value);
+            auto itr = accountstable.find(eden_fractal::eos_symbol.code().raw());
+            if (itr == accountstable.end()) {
+                return (int64_t)0;
+            }
+            else {
+                return itr->balance.amount;
+            }
+        };
+
+        THEN("James has 0 Eden and 0 EOS")
+        {  //
+            CHECK(getJamesEden() == 0);
+            CHECK(getJamesEOS() == 0);
+        }
+        WHEN("The ranking is submitted and James is rank 1")
+        {
+            dan.act<actions::submitranks>(ranks);
+            THEN("James has 5 EDEN")
+            {
+                CHECK(getJamesEden() == s2a("5.0000 EDEN").amount);
+
+                AND_THEN("James has 1.8238 EOS")
+                {  //
+                    CHECK(getJamesEOS() == s2a("1.8238 EOS").amount);
+                }
+            }
+        }
+
+        /*
+        void eosrewardamt(const asset& quantity);
+        void fiboffset(uint8_t offset);
+        */
+
+        WHEN("EOS reward changes to 200")
+        {
+            auto changeReward = dan.trace<actions::eosrewardamt>(s2a("200.0000 EOS"));
+            CHECK(succeeded(changeReward));
+
+            AND_WHEN("The ranking is submitted and James is rank 1")
+            {
+                dan.act<actions::submitranks>(ranks);
+                THEN("James has 5 EDEN")
+                {  //
+                    CHECK(getJamesEden() == s2a("5.0000 EDEN").amount);
+
+                    AND_THEN("James has 3.6477 EOS")
+                    {  //
+                        CHECK(getJamesEOS() == s2a("3.6477 EOS").amount);
+                    }
+                }
+            }
+        }
+        WHEN("The fib offset is changed to 6")
+        {
+            auto setFibOffset = dan.trace<actions::fiboffset>(6);
+            CHECK(succeeded(setFibOffset));
+            AND_WHEN("The ranking is submitted and James is rank 1")
+            {
+                dan.act<actions::submitranks>(ranks);
+
+                THEN("James has 8 EDEN")
+                {
+                    CHECK(getJamesEden() == s2a("8.0000 EDEN").amount);
+
+                    AND_THEN("James has 1.8238 EOS")
+                    {  //
+                        CHECK(getJamesEOS() == s2a("1.8238 EOS").amount);
+                    }
+                }
+            }
+        }
+    }
+}
+
+SCENARIO("Setting reward and fib offset")
+{
+    GIVEN("Standard setup")
+    {
+        // This starts a single-producer chain
+        test_chain t;
+        setup_installMyContract(t);
+        setup_createAccounts(t);
+        setup_token(t);
+
+        auto alice = t.as("alice"_n);
+
+        THEN("Alice cannot change the fib offset")
+        {
+            auto trace = alice.trace<actions::fiboffset>(8);
+            CHECK(failedWith(trace, requiresAdmin));
+        }
+        THEN("Alice cannot change the EOS reward amount")
+        {
+            auto trace = alice.trace<actions::eosrewardamt>(s2a("2000.0000 EOS"));
+            CHECK(failedWith(trace, requiresAdmin));
+        }
+    }
+}
